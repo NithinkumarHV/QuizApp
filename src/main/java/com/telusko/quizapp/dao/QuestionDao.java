70d9c7dbc2df2790602e9cecf14bb1b255704597
Nithin Kumar H V <nithinkumarhv@Nithins-MacBook-Air.local>
2023-07-23 11:43:07 +0530

    Initial commit

---
+++ src/main/java/com/telusko/quizapp/dao/QuestionDao.java
@@ -0,0 +1,22 @@
+package com.telusko.quizapp.dao;
+
+import com.telusko.quizapp.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/* Data JPA handles all the queries of the table and returns data via methods
+    parameters,
+    1) table name(class name) here, Question
+    2) primary key type. here, integer
+*/
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
+}
---
+++ src/main/java/com/telusko/quizapp/model/Quiz.java
@@ -0,0 +1,21 @@
+package com.telusko.quizapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    /* Creates a table called quiz_questions(quiz_id, question_id) */
+    @ManyToMany
+    private List<Question> questions;
+
+}
